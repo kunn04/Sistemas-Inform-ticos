a2edb03a26a7c2eff029fce7e300c3c89d7e9177
Luisnf5 <72856520+Luisnf5@users.noreply.github.com>
2024-09-25 09:41:42 +0200

    Add files via upload

---
+++ user.py
@@ -0,0 +1,53 @@
+from quart import Quart, jsonify, request
+import os, uuid, hashlib
+
+SECRET_UUID = "00010203-0405-0607-0809-0a0b0c0d0e0f""
+
+app = Quart(__name__)
+
+def generar_token(uid):
+    hash = uuid.uuid5(SECRET_UUID, str(uid))
+    return str(hash)
+
+def 
+
+@app.route('/home')
+async def index():
+    return 'Hello World'
+
+@app.route('/test1')
+async def test1():
+    exists = False
+    pwd = ''
+    with open('users.txt', 'r+') as f:
+        for line in f:
+            if line.split(':')[0] == 'jorge':
+                exists = True
+                pwd = line.split(':')[1]
+
+    if exists == True:
+        return pwd
+    else:
+        with open('users.txt', 'a') as f:
+            f.write('jorge:' + str(hashlib.sha1()))
+            os.mkdir('jorge')
+
+    return 'created'
+
+@app.route('/user/create_user', methods = ['PUT'])
+async def login():
+    datos = await request.get_json()
+    user = datos.get("nombre")
+    pwd = datos.get("password")
+    uid = str(uuid.uuid4())
+
+    with open('users.txt', 'a') as f:
+        f.write('\n' + user + ':' + pwd + ':' + uid)
+        os.mkdir(user)
+
+    return user
+    
+
+if __name__ == "__main__":
+    app.run(host='localhost', port=5050, debug=True)
+
