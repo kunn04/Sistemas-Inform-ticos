9266f21b33ac03613ec9c2675315ff7e046dee59
kunn04 <javieraguero104@gmail.com>
2024-11-11 18:57:46 +0100

    comentarios añadidos en actualiza.sql

--- Practica2/actualiza.sql
+++ Practica2/actualiza.sql
@@ -1,3 +1,4 @@
+-- Elimina los duplicados en orderdetail según orderid y prod_id, conservando solo el primero
 WITH cte AS (
     SELECT ctid, ROW_NUMBER() OVER (PARTITION BY orderid, prod_id ORDER BY ctid) AS rn
     FROM orderdetail
@@ -7,18 +8,22 @@ WHERE ctid IN (
     SELECT ctid FROM cte WHERE rn > 1
 );
 
+-- Define la clave primaria en orderdetail para orderid y prod_id
 ALTER TABLE orderdetail
 ADD CONSTRAINT pk_orderdetail
 PRIMARY KEY (orderid, prod_id);
 
+-- Establece una clave foránea en inventory que referencia a prod_id de products
 ALTER TABLE inventory
 ADD CONSTRAINT fk_prod_id
 FOREIGN KEY (prod_id) REFERENCES products(prod_id);
 
+-- Define la clave primaria en imdb_actormovies para actorid y movieid
 ALTER TABLE imdb_actormovies
 ADD CONSTRAINT pk_actormovies
 PRIMARY KEY (actorid, movieid);
 
+-- Añade las claves foráneas en orderdetail para orderid y prod_id
 ALTER TABLE orderdetail 
 ADD CONSTRAINT fk_orderid
 FOREIGN KEY (orderid) REFERENCES orders(orderid);
@@ -27,6 +32,7 @@ ALTER TABLE orderdetail
 ADD CONSTRAINT fk_prod_id
 FOREIGN KEY (prod_id) REFERENCES products(prod_id);
 
+-- Añade claves foráneas en imdb_actormovies para actorid y movieid
 ALTER TABLE imdb_actormovies
 ADD CONSTRAINT fk_actorid
 FOREIGN KEY (actorid) REFERENCES imdb_actors(actorid);
@@ -35,13 +41,16 @@ ALTER TABLE imdb_actormovies
 ADD CONSTRAINT fk_movieid
 FOREIGN KEY (movieid) REFERENCES imdb_movies(movieid);
 
+-- Agrega una clave foránea en orders que referencia customerid de customers
 ALTER TABLE orders
 ADD CONSTRAINT fk_customerid
 FOREIGN KEY (customerid) REFERENCES customers(customerid);
 
+-- Añade una columna balance en customers con un valor por defecto de 0.00
 ALTER TABLE customers 
 ADD COLUMN balance DECIMAL(10, 2) DEFAULT 0.00;
 
+-- Crea una tabla ratings para almacenar las valoraciones de los clientes a las películas
 CREATE TABLE ratings (
     customerid INT,
     movieid INT,
@@ -52,11 +61,11 @@ CREATE TABLE ratings (
     FOREIGN KEY (movieid) REFERENCES imdb_movies(movieid)
 );
 
-
+-- Amplía el tamaño de la columna password en customers
 ALTER TABLE customers 
 ALTER COLUMN password TYPE VARCHAR(128);
 
-
+-- Asigna a cada cliente un balance aleatorio hasta initialBalance
 CREATE OR REPLACE PROCEDURE setCustomersBalance (IN initialBalance BIGINT)
 LANGUAGE plpgsql
 AS $$
@@ -66,5 +75,5 @@ BEGIN
 END
 $$;;
 
-
-CALL setCustomersBalance(200)
+-- Llama al procedimiento para establecer balances aleatorios con un máximo de 200
+CALL setCustomersBalance(200);
